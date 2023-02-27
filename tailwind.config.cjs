c370cf5ffb23f8e7bbb3c4b5c5f29390a45a9a57
ichsanrchmn <ichsanugraha@gmail.com>
2023-02-28 01:40:18 +0700

    First Commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  mode: 'jit',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0a0a0a',
+        secondary: '#00f6ff',
+        dimWhite: 'rgba(255, 255, 255, 0.7)',
+        dimYellow: 'rgba(151, 129, 9, 0.1)',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+    screens: {
+      xs: '480px',
+      ss: '620px',
+      sm: '768px',
+      md: '1060px',
+      lg: '1200px',
+      xl: '1700px',
+    },
+  },
+  plugins: [],
+};
