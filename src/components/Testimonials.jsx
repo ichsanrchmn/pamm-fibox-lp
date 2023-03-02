e1b26e8246c3fc3bdcab737f15bb23fe6f350043
ichsanrchmn <ichsanugraha@gmail.com>
2023-03-02 17:29:02 +0700

    menambahkan carousel pada testimoni

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,13 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import styles from "../style";
 import { testi1, testi2, testi3, testi4 } from "../assets";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
 
 const Testimonials = () => {
   useEffect(() => {
     AOS.init();
   }, []);
+
+  const slides = [{ testi: testi1 }, { testi: testi2 }, { testi: testi3 }, { testi: testi4 }];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
 
   return (
     <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`} data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">
@@ -23,18 +45,25 @@ const Testimonials = () => {
         </div>
       </div>
 
-      <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600">
-        <div className="flex justify-between flex-col px-2 py-2 rounded-[10px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
-          <img src={testi1} alt="testimoni-pamm-fibox-gold" className="rounded-[10px] w-[350px]" />
+      <div className="max-w-[380px] w-full m-auto px-4 relative z-[5]">
+        <div className="w-full h-full rounded-lg bg-center bg-cover duration-500">
+          <img src={`${slides[currentIndex].testi}`} />
         </div>
-        <div className="flex justify-center flex-col px-2 py-2 rounded-[10px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
-          <img src={testi2} alt="testimoni-pamm-fibox-gold" className="rounded-[10px] w-[350px]" />
+        {/* Left Arrow */}
+        <div className="absolute top-[50%] translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+          <BsChevronCompactLeft onClick={prevSlide} size={30} />
         </div>
-        <div className="flex justify-center flex-col px-2 py-2 rounded-[10px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
-          <img src={testi3} alt="testimoni-pamm-fibox-gold" className="rounded-[10px] w-[350px]" />
+
+        {/* Right Arrow */}
+        <div className="absolute top-[50%] translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+          <BsChevronCompactRight onClick={nextSlide} size={30} />
         </div>
-        <div className="flex justify-center flex-col px-2 py-2 rounded-[10px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
-          <img src={testi4} alt="testimoni-pamm-fibox-gold" className="rounded-[10px] w-[350px]" />
+        <div className="flex top-4 justify-center py-2">
+          {slides.map((slide, slideIndex) => {
+            <div key={slideIndex} onClick={() => goToSlide(slideIndex)} className="text-2xl cursor-pointer z-[5]">
+              <RxDotFilled />
+            </div>;
+          })}
         </div>
       </div>
     </section>
